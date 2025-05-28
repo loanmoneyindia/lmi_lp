a603ab795fed81714265d95279725b5e1c5401b9
Priyanka K <priyanka.sony@gmail.com>
2025-05-28 16:25:15 +0530

    Fix the bug

--- src/app/apply-personal-loan/page.js
+++ src/app/apply-personal-loan/page.js
@@ -1,6 +1,5 @@
 
 import Image from "next/image";
-import ApplyPLForm from "@/components/apply-personal-loan/apply-form";
 
 import logo from "../../../public/images/logo.png";
 const page = () => {
@@ -9,7 +8,7 @@ const page = () => {
         <div id="_apply_pl" style={{ backgroundColor: "#cde5f7" }}>
             <div className="full_container">
                 <Image src={logo} alt="Logo" width={200} height={47} />
-                <ApplyPLForm />
+                
                 <p style={{ textAlign: "center", lineHeight: "1.4", paddingBottom: "20px", paddingTop: "8px", fontSize: "14px", fontWeight: "600", color: "#333"  }}>Copyright © 2025 loanmoney.co All Rights Reserved.</p>
             </div>
         </div>
