8d0af4be83cb3e428c347d40572fde89a06acdb3
Priyanka K <priyanka.sony@gmail.com>
2025-05-28 16:16:45 +0530

    Fix the bug

--- src/components/apply-personal-loan/apply-form.jsx
+++ src/components/apply-personal-loan/apply-form.jsx
@@ -1,38 +1,35 @@
 'use client'
-import { useSearchParams } from 'next/navigation'
 import CryptoJS from 'crypto-js'
-import { useEffect, useState } from 'react'
 
 const SECRET_KEY = '673C9BC13BC4E';
 
-const ApplyPLForm = () =>{
-    const searchParams = useSearchParams()
-    const [formData, setFormData] = useState({})
+const ApplyPLForm = ({ searchParams }) =>{
+ if (searchParams?.data) {
+    try {
+      const bytes = CryptoJS.AES.decrypt(decodeURIComponent(searchParams.data), SECRET_KEY)
+      const decrypted = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
+      mobile = decrypted.mobile
+      applicationno = decrypted.applicationno
+    } catch (err) {
+      console.error('Decryption failed:', err)
+    }
+  }
 
-    useEffect(() => {
-        const encryptedData = searchParams.get('data')
-        if (encryptedData) {
-        const decryptedBytes = CryptoJS.AES.decrypt(encryptedData, SECRET_KEY)
-        const decrypted = JSON.parse(decryptedBytes.toString(CryptoJS.enc.Utf8))
-        setFormData(decrypted)
-        }
-    }, [searchParams]);
-
-  const utm_source = searchParams.get('utm_source') || ''
-  const utm_medium = searchParams.get('utm_medium') || ''
+  const utm_source = searchParams?.utm_source || ''
+  const utm_medium = searchParams?.utm_medium || ''
 
     return(
         <div className="form-container">
             <h1 className="form-title">Apply for Personal Loan</h1>
             <p style={{ textAlign: "center", lineHeight: "1.4", paddingBottom: "30px", fontSize: "14px", fontWeight: "600", color: "#f5f8cf"  }}>Share your details and talk with our experts to get an instant decision on your loan.</p>
             <form>
-                <p>Application No: {formData.applicationno}</p>
+                <p>Application No: {applicationno}</p>
                 <div className="form-group">
                     <div className="form-field">
                         <input type="text" id="name" name="name" placeholder="Your name" />
                     </div>
                     <div className="form-field">
-                        <input type="text" id="phone" name="phone" placeholder="Mobile Number" value={formData.mobile} />
+                        <input type="text" id="phone" name="phone" placeholder="Mobile Number" value={mobile} />
                     </div>
                 </div>
                 <div className="form-group">
