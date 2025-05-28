7a956d0500d3c555751fe061c4203ddc20028639
Priyanka K <priyanka.sony@gmail.com>
2025-05-28 16:20:10 +0530

    Fix the bug

--- src/components/apply-personal-loan/apply-form.jsx
+++ src/components/apply-personal-loan/apply-form.jsx
@@ -10,6 +10,7 @@ const ApplyPLForm = ({ searchParams }) =>{
       const decrypted = JSON.parse(bytes.toString(CryptoJS.enc.Utf8))
       mobile = decrypted.mobile
       applicationNo = decrypted.applicationNo
+      console.log('Decrypted data:', decrypted)
     } catch (err) {
       console.error('Decryption failed:', err)
     }
@@ -23,13 +24,12 @@ const ApplyPLForm = ({ searchParams }) =>{
             <h1 className="form-title">Apply for Personal Loan</h1>
             <p style={{ textAlign: "center", lineHeight: "1.4", paddingBottom: "30px", fontSize: "14px", fontWeight: "600", color: "#f5f8cf"  }}>Share your details and talk with our experts to get an instant decision on your loan.</p>
             <form>
-                <p>Application No: {applicationNo}</p>
                 <div className="form-group">
                     <div className="form-field">
                         <input type="text" id="name" name="name" placeholder="Your name" />
                     </div>
                     <div className="form-field">
-                        <input type="text" id="phone" name="phone" placeholder="Mobile Number" value={mobile} />
+                        <input type="text" id="phone" name="phone" placeholder="Mobile Number" />
                     </div>
                 </div>
                 <div className="form-group">
